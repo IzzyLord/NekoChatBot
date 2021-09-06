fbc876929453f0d3e5ca10afc71b83d567f3344c
IzzyLord <89708171+IzzyLord@users.noreply.github.com>
2021-09-07 06:34:42 +0700

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -10,11 +10,11 @@ if HEROKU:
 # NOTE Fill this if you are not deploying on heroku.
 if not HEROKU:
 
-    bot_token = "16901971:AAFqdM_SQE1PB2P1xLr67k"
+    bot_token = "1996086754:AAEDGgUAaaQWSZ_q6d-7fuUe70JJGjUejqA"
     ARQ_API_KEY = "Get this from @ARQRobot"
 # List of supported languages >>
 # https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
-    LANGUAGE = "en"
+    LANGUAGE = "id"
 
 # Leave it as it is
 ARQ_API_BASE_URL = "https://thearq.tech"
